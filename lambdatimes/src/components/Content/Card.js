887cfc3e435487822c4a3cd812d8b596b3fdd02d
Mike Landers <landers.mike@gmail.com>
2019-01-18 09:25:50 -0800

    incomplete

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 const Card = props => {
-  return (
-    <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={'' /* image source goes here */} />
-        </div>
-        <span>By {/* author goes here */}</span>
-      </div>
-    </div>
-  );
+	return (
+		<div className="card">
+			<div className="headline">
+				{props.card.headline}
+				{/* headline goes here */}
+			</div>
+			<div className="author">
+				<div className="img-container">
+					<img
+						src={props.card.img /* image source goes here */}
+						alt="alt text"
+					/>
+				</div>
+				<span>
+					By {props.card.author}
+					{/* author goes here */}
+				</span>
+			</div>
+		</div>
+	);
 };
 
 // Make sure to include PropTypes.
